14086f9fa275902410eb80d3656712aa3117f5cd
BloodySteps <codewithtyson@gmail.com>
2021-11-19 00:26:29 -0800

    🎉deploy

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
 
       <div className="home">
         <div className="container">
-          <SearchBox />
+          <SearchBox placeholder="search location.."/>
           <FamousPlaces />
         </div>
       </div>
